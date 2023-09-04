e6a00572e69392f05b054653bb5c5a78c4fb66d3
Dharmesh Basapati Bacancy <dharmesh.basapati@bacancy.com>
2023-09-04 13:11:04 +0530

    Added required libraries and their plugins in respective build.gradle files.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("dagger.hilt.android.plugin")
+    id("kotlin-kapt")
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -27,11 +30,15 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
+    }
+    buildFeatures{
+        viewBinding = true
+        dataBinding = true
     }
 }
 
@@ -44,4 +51,51 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    val coroutinesFlowVersion = "1.6.4"
+    val coilVersion = "2.0.0-rc03"
+    val daggerHiltVersion = "2.42"
+    val hiltCompilerVersion = "1.0.0"
+    val lifecycleViewModelKTXVersion="2.6.1"
+    val roomVersion = "2.5.2"
+    val sdpVersion = "1.0.6"
+    val sspVersion = "1.0.6"
+    val retrofitVersion = "2.9.0"
+    val okhttpVersion = "4.9.3"
+
+    //Coroutines & Flow
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesFlowVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutinesFlowVersion")
+
+    //For using "by viewModels()"
+    implementation("androidx.fragment:fragment-ktx:1.6.1")
+
+    //Coil
+    implementation("io.coil-kt:coil:$coilVersion")
+
+    //hilt
+    implementation("com.google.dagger:hilt-android:$daggerHiltVersion")
+    kapt("com.google.dagger:hilt-compiler:$daggerHiltVersion")
+    kapt("androidx.hilt:hilt-compiler:$hiltCompilerVersion")
+
+    //Lifecycle related
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleViewModelKTXVersion")
+
+    //Room
+    implementation("androidx.room:room-runtime:$roomVersion")
+    implementation("androidx.room:room-ktx:$roomVersion")
+    ksp("androidx.room:room-compiler:$roomVersion")
+
+    //For using kotlin extensions of LiveData
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
+
+    //SDP and SSP for responsive design
+    implementation("com.intuit.sdp:sdp-android:$sdpVersion")
+    implementation("com.intuit.ssp:ssp-android:$sspVersion")
+
+    //Retrofit & okhttp
+    implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
+    implementation("com.squareup.retrofit2:converter-gson:$retrofitVersion")
+    implementation("com.squareup.okhttp3:okhttp:$okhttpVersion")
+    implementation("com.squareup.okhttp3:logging-interceptor:$okhttpVersion")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,18 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        val daggerHiltVersion = "2.42"
+        classpath("com.google.dagger:hilt-android-gradle-plugin:$daggerHiltVersion")
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle files
+    }
+}
 plugins {
     id("com.android.application") version "8.1.0" apply false
     id("org.jetbrains.kotlin.android") version "1.8.0" apply false
+    id("com.google.devtools.ksp") version "1.8.10-1.0.9" apply false
 }
